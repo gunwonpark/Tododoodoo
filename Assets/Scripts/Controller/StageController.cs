2a783c892bff73f5c0e1ab0468068345562daff0
leejh0469 <leejh0469@naver.com>
2024-01-26 12:26:22 +0900

    Comment - StageController 스크립트 주석 추가

--- Assets/Scripts/Controller/StageController.cs
+++ Assets/Scripts/Controller/StageController.cs
@@ -18,13 +18,21 @@ public class StageController : MonoBehaviour
     [SerializeField] private float _spawnLaserDelay;
     [SerializeField] private int _typeOfMonster;
 
+    // 스폰 관련 변경된 변수
     private int _currentSpawnNum;
     private float _currentSpawnDelay;
     private float _currentSpawnLaserDelay;
     private int _currentTypeOfMonster;
 
+    // 스폰 관련 변수 변경 시 적용할 값
+    [Header("Applied Value")]
+    [SerializeField] private int _addSpawnNum;
+    [SerializeField] private float _addSpawnDelay;
+
+    // 레일에 동시에 생성할 시 중복되지 않은 랜덤한 레일 인덱스 저장
     private List<int> spawnPointIndex = new List<int>();
 
+    // 생성할 몬스터 타입 이름
     private string[] spawnMonsterType = { "Monster_Common", "Monster_Range", "Monster_Rush" };
 
     private void Awake()
@@ -67,13 +75,14 @@ public class StageController : MonoBehaviour
         ObjectPool.i.DestroyAll("Block");
     }
 
+    // 스테이지에 따른 스폰관련 변수 변경
     void InitStageValue(int stageCount)
     {
         if(stageCount % 2 == 0)
         {
             if(_currentSpawnNum < 5)
             {
-                _currentSpawnNum++;
+                _currentSpawnNum += _addSpawnNum;
                 _currentSpawnDelay = _spawnDelay;
             }
             if(_currentTypeOfMonster < spawnMonsterType.Length)
@@ -83,10 +92,17 @@ public class StageController : MonoBehaviour
         }
         else
         {
-            _currentSpawnDelay -= .2f;
+            _currentSpawnDelay -= _addSpawnDelay;
         }
     }
 
+    /// <summary>
+    /// 범위 안의 랜덤한 값을 개수만큼 뽑아 리스트로 반환
+    /// </summary>
+    /// <param name="minValue"> 최소값(포함) </param>
+    /// <param name="maxValue"> 최대값(포함 x) </param>
+    /// <param name="num"> 뽑는 개수 </param>
+    /// <returns></returns>
     private List<int> GetRandomIndexList(int minValue, int maxValue, int num)
     {
         List<int> list = new List<int>();
@@ -107,6 +123,7 @@ public class StageController : MonoBehaviour
         return list;
     }
 
+    // 몬스터 스폰 코루틴
     IEnumerator SpawnMonster()
     {
         while(true)
@@ -122,14 +139,14 @@ public class StageController : MonoBehaviour
                 mc.Setup(obstacleSpawner, GameManager.Instance.Player);
             }
 
-            yield return new WaitForSeconds(_spawnDelay);
+            yield return new WaitForSeconds(_currentSpawnDelay);
         }
     }
     IEnumerator SpawnRazer() //맵 완성후 프리팹의 스케일 y값도 조절해야함
     {
         while (true)
         {
-            yield return new WaitForSeconds(_spawnLaserDelay);//레이저 주기
+            yield return new WaitForSeconds(_currentSpawnLaserDelay);//레이저 주기
             GameObject warringLine = ObjectPool.i.GetFromPool("WarringLine");
             warringLine.transform.position = GameManager.Instance.Player.position;//PlayerPosition;
             yield return new WaitForSeconds(1);// 경고 시간
