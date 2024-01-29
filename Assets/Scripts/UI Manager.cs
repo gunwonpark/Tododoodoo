e429545ca3f4c3ce579a130b717b6785545f3d5c
minsang95 <154496356+minsang95@users.noreply.github.com>
2024-01-29 15:53:21 +0900

    Style - 씬 매니지먼트 네임스페이스 삭제

--- Assets/Scripts/UI Manager.cs
+++ Assets/Scripts/UI Manager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
