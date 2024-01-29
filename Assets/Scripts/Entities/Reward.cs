5df49b86bae5397599c157d56e9a4537e7725b34
minsang95 <154496356+minsang95@users.noreply.github.com>
2024-01-29 15:00:06 +0900

    Fix - 플레이어 스텟 표시 업그레이드 카운트로 변경

--- Assets/Scripts/Entities/Reward.cs
+++ Assets/Scripts/Entities/Reward.cs
@@ -11,9 +11,13 @@ public class Reward : MonoBehaviour
     [SerializeField] private Button upgradeBtn2;
     [SerializeField] private Button upgradeBtn3;
 
-    public int attackUpgradeCount;
-    public int speedUpgradeCount;
-    public int jumpUpgradeCount;
+    [SerializeField] Text AttackDelay;
+    [SerializeField] Text MoveSpeed;
+    [SerializeField] Text JumpDegree;
+
+    private int attackUpgradeCount;
+    private int speedUpgradeCount;
+    private int jumpUpgradeCount;
 
     private void Awake()
     {
@@ -31,6 +35,7 @@ public class Reward : MonoBehaviour
     {
         GameManager.Instance.currentState = GameManager.State.Ready;
         Debug.Log("공격력 +" + ++attackUpgradeCount);
+        AttackDelay.text = attackUpgradeCount.ToString() + "+";
         rewardWindow.SetActive(false);
         Time.timeScale = 1f;
     }
@@ -38,14 +43,16 @@ public class Reward : MonoBehaviour
     public void Upgrade2()
     {
         GameManager.Instance.currentState = GameManager.State.Ready;
-        Debug.Log("공격력 +" + ++speedUpgradeCount);
+        Debug.Log("스피드 +" + ++speedUpgradeCount);
+        MoveSpeed.text = speedUpgradeCount.ToString() + "+";
         rewardWindow.SetActive(false);
         Time.timeScale = 1f;
     }
     public void Upgrade3()
     {
         GameManager.Instance.currentState = GameManager.State.Ready;
-        Debug.Log("공격력 +" + ++jumpUpgradeCount);
+        Debug.Log("점프력 +" + ++jumpUpgradeCount);
+        JumpDegree.text = jumpUpgradeCount.ToString() + "+";
         rewardWindow.SetActive(false);
         Time.timeScale = 1f;
     }
